2a24fa7e1fe7cf9b60ecf2e950eb7729874c4e0b
Augustin <raygister@gmail.com>
2023-11-15 10:59:17 +0100

    Etape 2 : Classe Orc qui étend Classe Creature

--- script.js
+++ script.js
@@ -12,4 +12,27 @@ class Creature {
     identity() {
       console.log(`I am a creature named ${this.name}`);
     }
-  }
+}
+
+class Orc extends Creature {
+    constructor(name, age, strength, hasRage) {
+      super(name, age, strength);
+      this.hasRage = hasRage;
+    }
+  
+    saysomething() {
+      console.log("Orc sound");
+    }
+  
+    identity() {
+      console.log(`I am an orc named ${this.name}`);
+    }
+  
+    scream() {
+      console.log("Ahhhhhhhhh!");
+    }
+  
+    bite() {
+      console.log("Orc biting");
+    }
+}
